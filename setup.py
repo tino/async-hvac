89abcfa4b70827d5362fa83595a26a8c28472455
Lionel Zerbib <lionel@alooma.io>
2018-06-11 12:59:51 +0300

    update versions

--- setup.py
+++ setup.py
@@ -25,11 +25,11 @@ setup(
     classifiers=['License :: OSI Approved :: Apache Software License'],
     packages=find_packages(),
     install_requires=[
-        'aiohttp==3.2.0',
+        'aiohttp==3.3.1',
     ],
     include_package_data=True,
     package_data={'async_hvac': ['version']},
     extras_require={
-        'parser': ['pyhcl>=0.2.1,<0.3.0']
+        'parser': ['pyhcl==0.3.10']
     }
 )
